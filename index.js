ea415628898338f4652f01872dfe1170080b079d
buhleqampi <buhle3857@gmail.com>
2024-04-24 09:27:52 +0200

    changes

--- index.js
+++ index.js
@@ -7,26 +7,23 @@
     // favoritesNav.addEventListener('click', () => {
     //     displayRecipes(favorites);
     // });
-    Promise.all([
-        fetch(recipesUrl).then(response => response.json()),
-        fetch(youtubeUrl).then(response => response.json())
-    ])
-        .then(([recipesData, youtubeData]) => {
-            allRecipes = recipesData.recipes.map((recipe, index) => {
+
+    fetch(recipesUrl)
+    .then(response => response.json())
+    .then(data => {
+        allRecipes = data.recipes;
+        displayRecipes(allRecipes);
+    }),
+    fetch(youtubeUrl)
+    .then(response => response.json())
+    .then(youtubeData => {
+            allRecipes = data.recipes.map((recipe, index) => {
                 recipe.youtubeLink = youtubeData.recipes[index].link;
                 return recipe;
             });
             displayRecipes(allRecipes);
         });
-  
-    const searchInput = document.getElementById('searchInput');
-  
-    searchInput.addEventListener('input', () => {
-        const searchTerm = searchInput.value.toLowerCase();
-        const filteredRecipes = allRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerm));
-        displayRecipes(filteredRecipes);
-    });
-  
+    
     function displayRecipes(recipes) {
         const recipesContainer = document.getElementById('recipes');
         recipesContainer.innerHTML = '';
@@ -36,7 +33,7 @@
             recipesContainer.appendChild(card);
         });
     }
-  
+
     function createRecipeCard(recipe) {
         const card = document.createElement('div');
         card.classList.add('recipe-card');
@@ -64,33 +61,28 @@
         });
         card.appendChild(readMoreBtn);
   
-        
-  
-        if (!favorites.includes(recipe)) {
-            const favoriteBtn = document.createElement('a');
-            favoriteBtn.innerHTML = '<i class="fas fa-heart fa-1xl" id="modal-icon"></i>';
-            favoriteBtn.addEventListener('click', () => {
-                addToFavorites(recipe);
-            });
-            card.appendChild(favoriteBtn);
-        } else {
-            const deleteBtn = document.createElement('a');
-            deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-            deleteBtn.addEventListener('click', () => {
-                removeFromFavorites(recipe.name);
-            });
-            card.appendChild(deleteBtn);
-        }
-  
+    if (!favorites.includes(recipe)) {
+        const favoriteBtn = document.createElement('a');
+        favoriteBtn.innerHTML = '<i class="fas fa-heart fa-1xl" id="modal-icon"></i>';
+        favoriteBtn.addEventListener('click', () => {
+            addToFavorites(recipe);
+        });
+        card.appendChild(favoriteBtn);
+    }else {
+        const deleteBtn = document.createElement('a');
+        deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+        deleteBtn.addEventListener('click', () => {
+            removeFromFavorites(recipe.name);
+        });
+        card.appendChild(deleteBtn);
+    }
         const youtubeLink = document.createElement('a');
         youtubeLink.innerHTML = '<i class="fa-regular fa-circle-play fa-1xl" id="modal-icon"></i>';
         youtubeLink.href = recipe.youtubeLink;
         youtubeLink.target = '_blank';
         card.appendChild(youtubeLink);
-  
         return card;
     }
-  
   
     function showMoreDetails(recipe) {
       const modal = document.getElementById('myModal');
@@ -105,8 +97,7 @@
                   <h1 class="modal-title"><strong>${recipe.name}</strong> (${recipe.mealType}) </h1>
                     <p><i class="fa-regular fa-clock"></i> Prep Time: ${recipe.prepTimeMinutes} min</p>   
                     <p><i class="fa-solid fa-clock"></i> Cook Time: ${recipe.cookTimeMinutes} min</p>
-                    <p><strong><i class="fa-solid fa-people-group"></i></strong> ${recipe.servings} people/person</p>
-                    
+                    <p><strong><i class="fa-solid fa-people-group"></i></strong> ${recipe.servings} people/person</p>                  
                     <div class="modal-btns">
                     <a><i class="fa-regular fa-circle-play fa-2xl"></i></a>
                     <a><i class="fas fa-heart fa-2xl"></i></a>
@@ -115,92 +106,17 @@
                   </div>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                  
                   <div class="modal-body">
                   <p>Tags: ${recipe.tags}</p>
                       <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
                       <p><strong>Instructions:</strong> ${recipe.instructions}</p>
-  
                       <p><strong>Calories per Serving:</strong> ${recipe.caloriesPerServing}</p>
-                  </div>
-                  
+                  </div>                 
               </div>
           </div>
       `;
-      const myModal = new bootstrap.Modal(modal);
-      myModal.show();
-  }
-  
-    const closeModalBtn = document.getElementsByClassName('close')[0];
-    const modal = document.getElementById('myModal');
-  
-  function mRead(ingredients){
-  
-    // Create New Speech Synthesis Utterance 
-  let utterance = new SpeechSynthesisUtterance(`Before we get you started 
-  lets make sure you have the following ingredients... ${ingredients}`);
-  utterance.rate = 0.9
-    
-  const synth = speechSynthesis;
-   
-   let voices = synth.getVoices()
-   
-       utterance.voice = voices[0];
-      
-      utterance.addEventListener("end", (event) => {
-  
-        setTimeout(() => {
-          
-          let utterance2 = new SpeechSynthesisUtterance(`Shall we begin?`);
-          utterance2.voice = voices[0];
-          utterance2.rate = 0.8
-          speechSynthesis.speak(utterance2);
-  
-          const user = confirm("Shall we begin?");
-  
-          if(user)
-          {
-              let utterance3 = new SpeechSynthesisUtterance(`Great let's begin.`);
-              utterance3.voice = voices[0];
-              utterance3.rate = 0.8
-              speechSynthesis.speak(utterance3);
-  
-              const ins = instructions.split(".,");
-              let nextStep = true;
-  
-              let counter = 0;
-  
-              let inter = setInterval(() => {
-  
-                if(nextStep)
-                {
-                  utterance3.text = `Step ${counter+1}, ${ins[counter]}`
-                  speechSynthesis.speak(utterance3);
-  
-                  nextStep = false
-                  counter++;
-  
-                  utterance3.addEventListener("end", (event) => {
-                    nextStep = true
-                  });
-                }
-  
-                if(counter == ins.length){
-  
-                  setTimeout(() => {
-                    utterance3.text = `Cooking complete, Well done!`
-                    speechSynthesis.speak(utterance3);
-                  }, 3000)
-                  
-                  clearInterval(inter);
-                }
-              }, 1500)
-            }
-        }, 1000)
-      });
-      
-      speechSynthesis.speak(utterance);
-  
+        const myModal = new bootstrap.Modal(modal);
+        myModal.show();
   }
 
     function getStars(rating) {
@@ -208,6 +124,76 @@
         const stars = '🔥'.repeat(roundedRating);
         return stars;
     }
+
+    function mRead(ingredients){
+    
+        // Create New Speech Synthesis Utterance 
+    let utterance = new SpeechSynthesisUtterance(`Before we get you started 
+    lets make sure you have the following ingredients... ${ingredients}`);
+    utterance.rate = 0.9
+        
+    const synth = speechSynthesis;
+    
+    let voices = synth.getVoices()
+    
+        utterance.voice = voices[0];
+        
+        utterance.addEventListener("end", (event) => {
+    
+            setTimeout(() => {
+            
+            let utterance2 = new SpeechSynthesisUtterance(`Shall we begin?`);
+            utterance2.voice = voices[0];
+            utterance2.rate = 0.8
+            speechSynthesis.speak(utterance2);
+    
+            const user = confirm("Shall we begin?");
+
+        if(user)
+        {
+            let utterance3 = new SpeechSynthesisUtterance(`Great let's begin.`);
+            utterance3.voice = voices[0];
+            utterance3.rate = 0.8
+            speechSynthesis.speak(utterance3);
+    
+            const ins = instructions.split(".,");
+            let nextStep = true;
+
+            let counter = 0;
+
+            let inter = setInterval(() => {
+
+                if(nextStep)
+                {
+                utterance3.text = `Step ${counter+1}, ${ins[counter]}`
+                speechSynthesis.speak(utterance3);
+
+                nextStep = false
+                counter++;
+    
+                utterance3.addEventListener("end", (event) => {
+                    nextStep = true
+                });
+            }
+
+            if(counter == ins.length){
+
+                setTimeout(() => {
+                utterance3.text = `Cooking complete, Well done!`
+                speechSynthesis.speak(utterance3);
+                }, 3000)
+                
+                clearInterval(inter);
+            }
+            }, 1500)
+        }
+    }, 1000)
+    });
+            
+            speechSynthesis.speak(utterance);
+  }
+
+  
   
   
     function addToFavorites(recipe) {
@@ -238,3 +224,11 @@
         displayRecipes(favorites);
     });
 // });
+
+const searchInput = document.getElementById('searchInput');
+  
+searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredRecipes = allRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerm));
+    displayRecipes(filteredRecipes);
+});
